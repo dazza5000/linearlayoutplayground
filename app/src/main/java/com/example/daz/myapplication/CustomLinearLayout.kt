35627bcc904122f6d7111b64ece08d4e706a9f2d
Darran Kelinske <darran.kelinske@gmail.com>
2018-07-02 22:12:45 -0500

    more experimentation

--- app/src/main/java/com/example/daz/myapplication/CustomLinearLayout.kt
+++ app/src/main/java/com/example/daz/myapplication/CustomLinearLayout.kt
@@ -2,14 +2,18 @@ package com.example.daz.myapplication
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 
 class CustomLinearLayout @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var scrollView: ScrollView
 
     init {
         orientation = VERTICAL
@@ -19,10 +23,37 @@ class CustomLinearLayout @JvmOverloads constructor(
             addView(view)
         }
         val button = Button(context)
-        button.setOnClickListener { for (i in 17 downTo 7) {
-            removeView(getChildAt(i))
-        }
+        button.setOnClickListener {
+            removeViews()
         }
         addView(button)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+    }
+
+    private var removing: Boolean = false
+
+    private fun removeViews() {
+        removing = true
+        scrollView = parent.parent as ScrollView
+        for (i in 17 downTo 7) {
+            var view = getChildAt(i)
+            view?.run {
+                Log.e("woot", "height of the view is:" + view.height)
+                scrollView.post {scrollView.scrollBy(0, -view.height)}
+            }
+            removeView(getChildAt(i))
+        }
+        removing = false
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+//        scrollView = parent.parent as ScrollView
+//        if (removing) {
+//            scrollView.post {scrollView.scrollBy(0, h - oldh)}
+//        }
     }
 }
